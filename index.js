81191b659ba8ea78e89d53bb4ec9bc8b0e4af850
JorgeEder <sudoorange@gmail.com>
2019-09-15 21:23:38 -0300

    Query Params

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.send({ message: 'Hello World' });
+// Query params = ?user=1
+// Route params = /users/1
+// Request Body = { "nome": "Jorge", "email": "sudoorange@gmail.com"}
+
+server.get('/user', (req, res) => {
+  const nome = req.query.nome;
+
+  return res.json({ message: `Hello ${nome}` });
 })
 
 server.listen(3000);
